1cce8c987886d3e93b67d7a34a38d2a74734a2af
OliveraM <olivera@3ap.ch>
2022-10-27 20:53:00 +0200

    cleanup

--- src/main/java/com/gamify/onboarding/GamifyOnboardingApplication.java
+++ src/main/java/com/gamify/onboarding/GamifyOnboardingApplication.java
@@ -1,30 +1,12 @@
 package com.gamify.onboarding;
 
-import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.api.domain.BasicIssue;
-import com.atlassian.jira.rest.client.api.domain.Issue;
-import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
-import com.gamify.onboarding.model.AsynchronousMyJiraRestClient;
-import com.gamify.onboarding.model.AsynchronousMyJiraRestClientFactory;
-import com.gamify.onboarding.model.MyJiraClient;
-import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GamifyOnboardingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamifyOnboardingApplication.class, args);
-
-		URI uri = URI.create("https://3apjira.atlassian.net");
-
-		JiraRestClient jiraRestClient = new AsynchronousMyJiraRestClientFactory()
-				.createWithBasicHttpAuthentication(URI.create("https://3apjira.atlassian.net"),
-						"nemanja.djokic@3ap.ch", "1CUjZLCbbsWc8xKXwx3b2467");
-		Issue task = jiraRestClient.getIssueClient().getIssue("EOS-02").claim();
-		System.out.println(task.getId());
 }
 };
--- src/main/java/com/gamify/onboarding/controller/MissionController.java
+++ src/main/java/com/gamify/onboarding/controller/MissionController.java
@@ -1,20 +1,22 @@
 package com.gamify.onboarding.controller;
 
 import com.gamify.onboarding.dto.MissionUpdate;
-import com.gamify.onboarding.model.Mission;
 import com.gamify.onboarding.dto.UserRequest;
+import com.gamify.onboarding.model.Mission;
 import com.gamify.onboarding.service.MissionService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
-@RestController("/missions")
+@RestController
+@RequestMapping("/missions")
 public class MissionController {
 
-    private MissionService missionService;
+    private final MissionService missionService;
 
     @GetMapping
     public ResponseEntity<List<Mission>> getAllUserMissions(@RequestBody UserRequest request) {
@@ -31,11 +33,8 @@ public class MissionController {
         missionService.updateMissionStatus(id, request.getMissionStatus(), request.getUsername());
     }
 
-    @GetMapping("/current")
+    @GetMapping("current")
     public Mission getCurrentMission(@RequestBody UserRequest request){
         return missionService.getCurrentUserMission(request.getUsername());
     }
-
-
-
 }
