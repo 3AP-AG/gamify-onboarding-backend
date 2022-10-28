a676638143389a1566a2c15798725fff29b8bf39
duska3ap <duska@3ap.ch>
2022-10-28 11:10:32 +0200

    POF for mapping and geting all issues

--- src/main/java/com/gamify/onboarding/GamifyOnboardingApplication.java
+++ src/main/java/com/gamify/onboarding/GamifyOnboardingApplication.java
@@ -2,11 +2,20 @@ package com.gamify.onboarding;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.SearchResult;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import com.gamify.onboarding.model.Mission;
+import com.gamify.onboarding.model.MissionStatus;
+import com.google.common.collect.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
 public class GamifyOnboardingApplication {
@@ -15,12 +24,31 @@ public class GamifyOnboardingApplication {
 
 		JiraRestClient jiraRestClient = new AsynchronousJiraRestClientFactory()
 				.createWithBasicHttpAuthentication(URI.create("https://3apjira.atlassian.net"),
-						"nemanja.djokic@3ap.ch", "MLL6f9VD3ZNZZlwz8nGxFEB3");
+						"duska.lazic.jakovljevic@3ap.ch", "VtyreaehKOMRkIIzGQxn65CD");
 
 		Issue task = jiraRestClient.getIssueClient().getIssue("EOS-2").claim();
 
-
 		System.out.println(task.getId());
+
+		SearchResult sr = jiraRestClient.getSearchClient().searchJql("assignee=632da4d561dbef2805be1c56 AND labels=gamify").claim();
+
+
+
+		Iterable<Issue> iterable = () -> sr.getIssues().iterator();
+		Stream<Issue> targetStream = StreamSupport.stream(iterable.spliterator(), false);
+
+		List<Mission> missions = targetStream
+				.map(issue ->
+						new Mission(issue.getKey(),
+						issue.getSummary(),
+						issue.getDescription(),
+						MissionStatus.TODO,
+						//TODO promeni na status
+						1)
+				).collect(Collectors.toList());;
+		System.out.println(missions.size());
+		System.out.println(missions.get(0).getStatus());
+
 	}
 
 };
