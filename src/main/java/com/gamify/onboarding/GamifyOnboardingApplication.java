2393ba9f559af2c5e919ff8bf4c517eff30ad311
duska3ap <duska@3ap.ch>
2022-10-28 10:29:15 +0200

    In progress - Added jira library and prototype for getting an issue by hardcoded key

--- src/main/java/com/gamify/onboarding/GamifyOnboardingApplication.java
+++ src/main/java/com/gamify/onboarding/GamifyOnboardingApplication.java
@@ -1,12 +1,26 @@
 package com.gamify.onboarding;
 
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
 
 @SpringBootApplication
 public class GamifyOnboardingApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(GamifyOnboardingApplication.class, args);
+
+		JiraRestClient jiraRestClient = new AsynchronousJiraRestClientFactory()
+				.createWithBasicHttpAuthentication(URI.create("https://3apjira.atlassian.net"),
+						"nemanja.djokic@3ap.ch", "MLL6f9VD3ZNZZlwz8nGxFEB3");
+
+		Issue task = (jiraRestClient.getIssueClient()).getIssue("EOS-2").claim();
+
+
+		System.out.println(task.getId());
 	}
 
 };
